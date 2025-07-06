da484a3b8c12cdfa99a12bbcd10e075c3464b19a
Stillnox <ozeki1@gmail.com>
2025-07-05 21:55:31 -0300

    Update MainLayout.kt

--- MainLayout.kt
+++ MainLayout.kt
@@ -2,7 +2,13 @@ package com.example.todolist
 
 import android.content.SharedPreferences
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
